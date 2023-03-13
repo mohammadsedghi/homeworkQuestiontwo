24d8822b156dcf68f27d420c21317f1a002d5317
mohammad <mohammadsedghi1993@gmail.com>
2023-03-13 20:04:50 +0330

    three type of scanner method is created

--- ElementArray.java
+++ ElementArray.java
@@ -1,8 +1,25 @@
 package questiontwo;
 
+import java.util.Scanner;
+
 public class ElementArray {
  MetalElement [] metalElementsArray=new MetalElement[2];
  NonMetalElement[] nonMetalElementsArray=new NonMetalElement[2];
- 
+ public  String scannerString(String type){
+     Scanner scanner=new Scanner(System.in);
+     System.out.println(type);
+     return scanner.next();
+ }
+    public  int scannerAtomicNumber(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("ATOMIC NUMBER :");
+        return scanner.nextInt();
+    }
+    public  double scannerAtomicWeight(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("ATOMIC WEIGHT :");
+        return scanner.nextDouble();
+    }
+
 
 }
