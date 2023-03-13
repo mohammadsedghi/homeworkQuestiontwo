ecad1b4035d63f2658e5e9924e92a4c377445073
mohammad <mohammadsedghi1993@gmail.com>
2023-03-13 21:52:14 +0330

    this app in totall class debuged and run correctly

--- ElementArray.java
+++ ElementArray.java
@@ -18,7 +18,7 @@ public class ElementArray {
         Scanner scanner = new Scanner(System.in);
         System.out.println();
         System.out.println("enter metal or non metal");
-        return scanner.next();
+        return scanner.nextLine();
     }
 
     public String scannerNameOfElement() {
@@ -41,17 +41,15 @@ public class ElementArray {
     }
 
     public void addElement() {
-        if (scannerString().equals(" metal")) {
-            // MetalElement metalElement=new MetalElement("Li",3,6.941);
-            // MetalElement metalElement1=new MetalElement("Na",11,22.989769);
+        String str=scannerString();
+        if (str.equals("metal")) {
             MetalElement metalElement = new MetalElement(scannerNameOfElement(), scannerAtomicNumber(), scannerAtomicWeight());
             for (int i = 0; i < metalElementsArray.length; i++) {
                 if (metalElementsArray[i] == null) metalElementsArray[i] = metalElement;
             }
         }
-        if (scannerString().equals("non metal")) {
-            // NonMetalElement nonMetalElement=new NonMetalElement("He",2,4.002602);
-            //  NonMetalElement nonMetalElement1=new NonMetalElement("N",7,14.0067);
+       else if (str.equals("non metal")) {
+
             NonMetalElement nonMetalElement=new NonMetalElement(scannerNameOfElement(),scannerAtomicNumber(),scannerAtomicWeight());
             for (int i = 0; i < nonMetalElementsArray.length; i++) {
                 if (nonMetalElementsArray[i] == null) nonMetalElementsArray[i] = nonMetalElement;
--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
 package questiontwo;
 
 public class Main {
+    // MetalElement metalElement=new MetalElement("Li",3,6.941);
+    // MetalElement metalElement1=new MetalElement("Na",11,22.989769);
+    // NonMetalElement nonMetalElement=new NonMetalElement("He",2,4.002602);
+    //  NonMetalElement nonMetalElement1=new NonMetalElement("N",7,14.0067);
     public static void main(String[] args) {
         ElementArray elementArray=new ElementArray();
-        for (int i = 0; i <4 ; i++) {
+        for (int i = 0; i <4; i++) {
             elementArray.addElement();
+
         }
+        System.out.println("===================================================");
         for (MetalElement metalElement:elementArray.getMetalElementsArray()
-             ) {
-            metalElement.describeElement();
-        }
-        for (NonMetalElement NonMetal:elementArray.getNonMetalElementsArray()
         ) {
-           NonMetal.describeElement();
+            if (metalElement!=null) {
+                metalElement.describeElement();
+            }
+        }
+        for (NonMetalElement nonMetal:elementArray.getNonMetalElementsArray()
+        ) {
+            if (nonMetal!=null) {
+                nonMetal.describeElement();
+            }
         }
     }
 
