835dfe25621ccc8f3153a68364a6ffa67e3868a4
mohammad <mohammadsedghi1993@gmail.com>
2023-03-13 19:31:14 +0330

    brief of MetalElement is added

--- Element.java
+++ Element.java
@@ -4,6 +4,7 @@ public abstract class Element {
    private String symbol;
     private int atomicNumber;
     private double atomicWeight;
+
     public Element(String symbol,int atomicNumber,double atomicWeight){
         this.symbol=symbol;
         this.atomicNumber=atomicNumber;
--- MetalElement.java
+++ MetalElement.java
@@ -1,14 +1,17 @@
 package questiontwo;
 
-public class MetalElement extends Element{
+public class MetalElement extends Element {
+
     public MetalElement(String symbol, int atomicNumber, double atomicWeight) {
         super(symbol, atomicNumber, atomicWeight);
     }
 
     @Override
     public void describeElement() {
-        System.out.println("SYMBOL :"+getSymbol());
-        System.out.println("ATOMIC NUMBER :"+getAtomicNumber());
-        System.out.println("ATOMIC WEIGHT :"+getAtomicWeight());
+        System.out.println("SYMBOL :" + getSymbol());
+        System.out.println("ATOMIC NUMBER :" + getAtomicNumber());
+        System.out.println("ATOMIC WEIGHT :" + getAtomicWeight());
+        System.out.println("brief of element is : Metals are good conductors of heat and electricity," +
+                " and are malleable (they can be hammered into sheets)");
     }
 }
