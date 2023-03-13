d0e9c8a65bbf2257a6b073e39664ceb6ebba7d66
mohammad <mohammadsedghi1993@gmail.com>
2023-03-13 19:43:10 +0330

    MetalElementArray and NonMetalElements created

--- ElementArray.java
+++ ElementArray.java
@@ -1,5 +1,8 @@
 package questiontwo;
 
 public class ElementArray {
-    
+ MetalElement [] metalElementsArray=new MetalElement[2];
+ NonMetalElement[] nonMetalElementsArray=new NonMetalElement[2];
+ 
+
 }
