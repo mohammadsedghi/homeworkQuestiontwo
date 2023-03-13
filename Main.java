66825676cb03042717bcf43e0177dff60221d9d8
mohammad <mohammadsedghi1993@gmail.com>
2023-03-13 20:27:59 +0330

    instance of ElementArray and its addElement method added to Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 package questiontwo;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        ElementArray elementArray=new ElementArray();
+        for (int i = 0; i <4 ; i++) {
+            elementArray.addElement();
+        }
+    }
+
 }
